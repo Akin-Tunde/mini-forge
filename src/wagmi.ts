670d4b812fd5ab97f6f2df2c1b665216b0578761
Pradise2 <akinsunmitaye@gmail.com>
2025-06-17 21:07:40 +0100

    first commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,10 @@
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { farcasterFrame as miniAppConnector } from '@farcaster/frame-wagmi-connector'
 import { http, createConfig } from "wagmi";
 import { base, mainnet } from "wagmi/chains";
 
 export const config = createConfig({
   chains: [base, mainnet],
-  connectors: [farcasterFrame()],
+  connectors: [miniAppConnector()],
   transports: {
     [base.id]: http(),
     [mainnet.id]: http(),
